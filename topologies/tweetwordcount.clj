588a82298a4ee70bf208ec01ba7ab9fa4c05864a
Jill Zhang <Jill Zhang>
2017-04-10 13:15:30 -0400

    final commit

--- topologies/tweetwordcount.clj
+++ topologies/tweetwordcount.clj
@@ -10,7 +10,7 @@
           options
           "spouts.tweets.Tweets"
           ["tweet"]
-          :p 1
+          :p 3
           )
     }
     ;; bolt configuration
@@ -19,7 +19,7 @@
           {"tweet-spout" :shuffle}
           "bolts.parse.ParseTweet"
           ["word"]
-          :p 2
+          :p 3
           )
      "count-bolt" (python-bolt-spec
           options
